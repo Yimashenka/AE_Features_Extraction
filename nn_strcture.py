3dde4e59088b33d757d74597f8ef4612d6fa75ff
Yimashenka <angeloni.datascience@gmail.com>
2022-11-08 14:40:59 +0100

    Adding the result of the training in results.txt

--- nn_strcture.py
+++ nn_strcture.py
@@ -5,7 +5,7 @@ import numpy as np
 from torch import nn, optim
 from torch.autograd import Variable
 
-from preprocessing_data import nb_users, training_set, nb_movies
+from preprocessing_data import nb_users, training_set, nb_movies, test_set
 
 sae = SAE.SAE()
 criterion = nn.MSELoss()
@@ -35,3 +35,19 @@ for epoch in range(1, nb_epoch + 1):
             s += 1.
             optimizer.step()
     print(f'epoch: {epoch}     loss: {train_loss/s}')
+
+# Testing the SAE
+test_loss = 0
+s = 0.
+for id_user in range(nb_users):
+    input = Variable(training_set[id_user]).unsqueeze(0)
+    target = Variable(test_set[id_user])
+    if torch.sum(target.data > 0) > 0:
+        output = sae(input)
+        target.require_grad = False
+        output[target == 0] = 0
+        loss = criterion(output, target)
+        mean_corrector = nb_movies/float(torch.sum(target.data > 0 ) +1e-10)
+        test_loss += np.sqrt(loss.data * mean_corrector)
+        s += 1.
+print(f'test loss: {test_loss/s}')
